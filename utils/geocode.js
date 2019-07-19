4bf14fe7ec74b071e5d42380a90b54cbaf27412d
anay-jain <anayjain98@gmail.com>
2019-07-19 23:45:31 +0530

    callback

--- utils/geocode.js
+++ utils/geocode.js
@@ -2,6 +2,7 @@ var request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?limit=1&access_token=pk.eyJ1IjoiYW5heS1qYWluIiwiYSI6ImNqeTB6dzZ0NDA3eXYzb3Bjd25pcmo0eW4ifQ.X9zI13hZFIrTl84wqu2xGA'
+    // hre url is url = url . we have used callback function
     request({url , json:true} , (error , {body}) => {
         if(error){
            callback("Please check your internet connection" , undefined);}
